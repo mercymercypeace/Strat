022435949d4e003bf5d6cf1494254971bd8f5c96
mercymercypeace <mercymercypeace@gmail.com>
2025-11-02 12:55:24 +0700

    Add files via upload

---
+++ ui/example.lua
@@ -0,0 +1,172 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/mercymercypeace/Strat/refs/heads/main/ui/gui.lua"))()
+
+local Window = Library:Window({
+    Title = "LunarisX",
+    Desc = "best script fr",
+    Icon = 127229998224259,
+    Theme = "Dark",
+    Version = "v1.0.0",
+    DiscordLink = "https://discord.gg/ygmYdN2GJY",
+    Config = {
+        Keybind = Enum.KeyCode.LeftControl,
+        Size = UDim2.new(0, 500, 0, 400)
+    },
+    CloseUIButton = {
+        Enabled = true,
+        Text = "LunarisX"
+    }
+})
+
+local Tab = Window:Tab({Title = "Main", Icon = "star"}) do
+    Tab:Section({Title = "Enable Feature"})
+    Tab:Toggle({
+        Title = "Enable Feature",
+        Desc = "toggle to enable or disable",
+        Value = false,
+        Callback = function(v)
+            print("toggle:", v)
+        end
+    })
+
+    Tab:Section({Title = "Run Action"})
+    Tab:Button({
+        Title = "Run Action",
+        Desc = "click to do something",
+        Callback = function()
+            print("button clicked!")
+            Window:Notify({
+                Title = "Button",
+                Desc = "action done lol",
+                Time = 3,
+                Type = "normal"
+            })
+        end
+    })
+
+    Tab:Section({Title = "Input Text"})
+    Tab:Textbox({
+        Title = "Input Text",
+        Desc = "type something here",
+        Placeholder = "enter value",
+        Value = "",
+        ClearTextOnFocus = false,
+        Callback = function(text)
+            print("textbox:", text)
+        end
+    })
+
+    Tab:Section({Title = "Set Speed"})
+    Tab:Slider({
+        Title = "Set Speed",
+        Min = 0,
+        Max = 100,
+        Rounding = 0,
+        Value = 25,
+        Callback = function(val)
+            print("slider:", val)
+        end
+    })
+
+    Tab:Section({Title = "Choose Option"})
+    Tab:Dropdown({
+        Title = "Choose Option",
+        List = {"Option 1", "Option 2", "Option 3"},
+        Value = "Option 1",
+        Callback = function(choice)
+            print("selected:", choice)
+        end
+    })
+
+    Tab:Section({Title = "Example Code"})
+    local CodeBlock = Tab:Code({
+        Title = "Example Code",
+        Code = "-- this is code\nprint('hello world')"
+    })
+
+    task.delay(5, function()
+        CodeBlock:SetCode("-- updated!\nprint('new stuff')")
+    end)
+end
+
+Window:Line()
+
+local Extra = Window:Tab({Title = "Extra", Icon = "tag"}) do
+    Extra:Section({Title = "Show Message"})
+    Extra:Button({
+        Title = "Show Message",
+        Desc = "show a popup",
+        Callback = function()
+            Window:Notify({
+                Title = "LunarisX",
+                Desc = "everything works!",
+                Time = 3,
+                Type = "normal"
+            })
+        end
+    })
+end
+
+Window:Line()
+
+local Settings = Window:Tab({Title = "Settings", Icon = "wrench"}) do
+    Settings:Section({Title = "Show Message"})
+    Settings:Button({
+        Title = "Show Message",
+        Desc = "display popup",
+        Callback = function()
+            Window:Notify({
+                Title = "LunarisX",
+                Desc = "settings page!",
+                Time = 3,
+                Type = "normal"
+            })
+        end
+    })
+
+    Settings:Section({Title = "Test Notifications"})
+    Settings:Button({
+        Title = "Error Notification",
+        Desc = "show error",
+        Callback = function()
+            Window:Notify({
+                Title = "Error",
+                Desc = "something went wrong!",
+                Time = 3,
+                Type = "error"
+            })
+        end
+    })
+
+    Settings:Button({
+        Title = "Warning Notification",
+        Desc = "show warning",
+        Callback = function()
+            Window:Notify({
+                Title = "Warning",
+                Desc = "be careful!",
+                Time = 3,
+                Type = "warning"
+            })
+        end
+    })
+
+    Settings:Button({
+        Title = "Normal Notification",
+        Desc = "show normal",
+        Callback = function()
+            Window:Notify({
+                Title = "Info",
+                Desc = "this is a normal notification",
+                Time = 3,
+                Type = "normal"
+            })
+        end
+    })
+end
+
+Window:Notify({
+    Title = "LunarisX",
+    Desc = "all components loaded!",
+    Time = 4,
+    Type = "normal"
+})
